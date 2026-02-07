ed48e375fb03333ed2ca269ab39e23b55f3896a2
Danny <144876407+TrAn-ThVy@users.noreply.github.com>
2026-02-07 11:39:42 +0700

    Add files via upload

---
+++ assets/js/title.js
@@ -0,0 +1,22 @@
+var titles = [
+  "@",
+  "@T",
+  "@Th",
+  "@ThV",
+  "@ThVy",
+  "@ThV",
+  "@Th",
+  "@T",
+  "@",	
+];
+
+function changeTitle() {
+  var index = 0;
+
+  setInterval(function() {
+      document.title = titles[index];
+      index = (index + 1) % titles.length;
+  }, 1000);
+}
+
+changeTitle();
